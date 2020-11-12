c071433d1b3ab82386c3d0fd6042402793a12f59
Louis Orleans <louis@orleans.io>
2020-11-11 19:50:11 -0800

    🚀 v1.0.0
    Done!

---
+++ pages/spinner.js
@@ -0,0 +1,11 @@
+import SVG from 'react-inlinesvg';
+import styles from '../styles/spinner.module.scss';
+// import loading_spinner from '../public/loading-spinner.svg';
+
+export default function Spinner() {
+  return (
+    <div className={styles.container}>
+      <SVG src="/loading-spinner.svg" className="spinner"></SVG>
+    </div>
+  );
+}
